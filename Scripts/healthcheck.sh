eedc65895274ff79d58933568538cb31a084cc88
Björn Kjeholt <dev@kjeholt.se>
2017-02-28 14:22:24 +0100

    Update healthcheck.sh

--- Scripts/healthcheck.sh
+++ Scripts/healthcheck.sh
@@ -6,9 +6,10 @@ DB_HOST_PORT_NO=3306
 DB_USER=root
 DB_PASSWORD=$MYSQL_ROOT_PASSWORD
 
-args=(
 	# force mysql to not use the local "mysqld.sock" (test "external" connectibility)
+args=(
 	-h"${DB_HOST_IP_ADDR}"
+	-p"${DB_HOST_PORT_NO}"
 	-u"${DB_USER}"
 	--silent
 )
